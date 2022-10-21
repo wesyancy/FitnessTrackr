4f9c88d6ffebe8a253ed41ce5a1083af8ae5cf21
Wes Yancy <79019393+wesyancy@users.noreply.github.com>
2022-10-20 23:09:27 -0500

    Updated to match Sean latest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-
-
+import '.public/style.css';
 
 const app = document.querySelector('#app');
 const root = ReactDOM.createRoot(app);
