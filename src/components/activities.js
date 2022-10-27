a7567a8e61b8aa56663515c1264a92e3ca7ee6d2
Sean <seanhan18@hotmail.com>
2022-10-26 23:37:17 -0500

    Updated Create Activity button to only appear when user is logged in

--- src/components/activities.js
+++ src/components/activities.js
@@ -18,7 +18,7 @@ const Activities = ({ activities, navigate, fetchActivities }) => {
     const filteredActivities = activities.filter(activity => activityMatches(activity, searchTerm));
     const activitiesToDisplay = searchTerm.length ? filteredActivities : activities;
 
-    if (searchTerm.length) {
+    if (searchTerm.length && token) {
         return (
             <Fragment>
                 <span id='activityHead'>
@@ -31,11 +31,13 @@ const Activities = ({ activities, navigate, fetchActivities }) => {
                             placeholder="Search"
                             onChange={(event) => setSearchTerm(event.target.value)}
                         />
-                        <Link to='/createActivity'>
-                            <button id='createActivityButton'>
-                                Create Activity
-                            </button>
-                        </Link>
+                        {
+                            <Link to='/createActivity'>
+                                <button id='createActivityButton'>
+                                    Create Activity
+                                </button>
+                            </Link>
+                        }
                     </form>
                 </span>
                 {
@@ -56,8 +58,7 @@ const Activities = ({ activities, navigate, fetchActivities }) => {
             </Fragment>
         )
     }
-    else {
-
+    else if (token) {
         return (
             <Fragment>
                 <span id='activityHead'>
@@ -79,6 +80,72 @@ const Activities = ({ activities, navigate, fetchActivities }) => {
                 </span>
                 {
                     activities.map((activity) => {
+                        const { name, description, id } = activity
+                        return (
+                            <main>
+                                <div id='Activities'>
+                                    <div id='Activity' key={id}>
+                                        <h3 id='ActivityName'>{name}</h3>
+                                        <p><strong>Description</strong>{description}</p>
+                                    </div>
+                                </div>
+                            </main>
+                        )
+                    })
+                }
+            </Fragment>
+        )
+    }
+    else if (searchTerm.length) {
+        return (
+            <Fragment>
+                <span id='activityHead'>
+                    <form className='top' onSubmit={(event) => {
+                        event.preventDefault()
+                    }}>
+                        <input
+                            className="searchbar"
+                            type='text'
+                            placeholder="Search"
+                            onChange={(event) => setSearchTerm(event.target.value)}
+                        />
+                    </form>
+                </span>
+                {
+                    activitiesToDisplay.map((activity) => {
+                        const { name, description, id } = activity
+                        return (
+                            <main>
+                                <div id='Activities'>
+                                    <div id='Activity' key={id}>
+                                        <h3 id='ActivityName'>{name}</h3>
+                                        <p><strong>Description</strong>{description}</p>
+                                    </div>
+                                </div>
+                            </main>
+                        )
+                    })
+                }
+            </Fragment>
+        )
+    }
+    else {
+        return (
+            <Fragment>
+                <span id='activityHead'>
+                    <form className='top' onSubmit={(event) => {
+                        event.preventDefault()
+                    }}>
+                        <input
+                            className="searchbar"
+                            type='text'
+                            placeholder="Search"
+                            onChange={(event) => setSearchTerm(event.target.value)}
+                        />
+                    </form>
+                </span>
+                {
+                    activitiesToDisplay.map((activity) => {
                         const { name, description, id } = activity
                         return (
                             <main>
